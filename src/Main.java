379b2d30d6867bb651fee6bc28d22504e060c093
TheFatPanda97 <shawnhu97@gmail.com>
2018-06-10 13:01:57 -0400

    Jun 10
    - now its a wrap, fogot main class

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,20 @@
 
 /**
  * Shawn Hu
- *
+ * Finished Jun 10
+ * Fight Club
  * */
 public class Main {
 
-    public static Read_Write rw = new Read_Write();
-    public static Fight_Club fightWindow = new Fight_Club();
-    public static Character_Select CSelectWindow = new Character_Select();
-    public static High_Score ScoreWindow = new High_Score();
-    public static Intro introWindow = new Intro();
+    public static Read_Write rw = new Read_Write();//File IO
+    public static Fight_Club fightWindow = new Fight_Club();//Actual fighting window
+    public static Character_Select CSelectWindow = new Character_Select();//character selection window
+    public static High_Score ScoreWindow = new High_Score();//high score window
+    public static Intro introWindow = new Intro();//main menu winodw
 
     public static void main(String[] args) {
 
+        //display the main menu window
         introWindow.setVisible(true);
 
     }
